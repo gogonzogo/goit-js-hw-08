d7118d9b3c332acacd3c8634bd0789a2feb476d2
gogonzogo <114868158+gogonzogo@users.noreply.github.com>
2023-02-08 09:53:22 -0500

    Revert "Task 1 first attempt"

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,37 +1,5 @@
-"use strict";
-
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// HTML REFRENCES
-const ref = {
-    galleryList: document.querySelector('.gallery'),
-};
-
-// MARKUP FUNCTION
-function galleryMarkup(array) {
-    return array.map((item) =>
-        `<a class="gallery__link" href="${item.original}"><img class="gallery__image"src="${item.preview}" alt="${item.description}" width="340"/></a>`).join('');
-}
-
-ref.galleryList.insertAdjacentHTML("afterbegin", galleryMarkup(galleryItems));
-
-// EVENT LISTENER AND FUNCTION
-ref.galleryList.addEventListener("click", handleClick);
-
-function handleClick(e) {
-    e.preventDefault();
-    const clickTarget = e.target;
-
-    if (clickTarget.nodeName !== "IMG") {
-        return;
-    };
-
-    let showLightbox = new SimpleLightbox('.gallery a');
-
-    showLightbox.on('show.simplelightbox', function () { });
-}
+console.log(galleryItems);
