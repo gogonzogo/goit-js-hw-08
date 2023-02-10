3c777d47830ffee1a58d65b178e87a392aedf609
gogonzogo <114868158+gogonzogo@users.noreply.github.com>
2023-02-10 05:37:32 +0000

    Deploying to gh-pages from @ gogonzogo/goit-js-hw-08@cad423d88f12a8e39f40190cd501441ca0b3e778 🚀

--- 02-video.14dc8b9b.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";var e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}))}();
-//# sourceMappingURL=02-video.14dc8b9b.js.map
