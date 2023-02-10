6b47cf98aab1eed8aa2c71f42baf193e03a41fc4
Jose Gonzales <jose.gonzales08@ymail.com>
2023-02-10 13:32:30 -0500

    task 3 update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+const ref = {
+  form: document.querySelector('form'),
+}
 
+let throttledOnInput = _.throttle(handleInput, 500);
+
+ref.form.addEventListener("input", onInput);
+
+function onInput(event) {
+  event.preventDefault();
+  
+
+
+}
