f138440c5023975f838dd63dffe00223119ffe1d
Jose Gonzales <jose.gonzales08@ymail.com>
2023-02-10 09:50:38 -0500

    task 2 update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,27 +8,18 @@ player.on('timeupdate', function (currentTime) {
   localStorage.setItem("videoplayer-current-time", vimeoCurrentTime);
 });
 
+
 try {
   const currentTimeObj = localStorage.getItem("videoplayer-current-time");
   const parsedCurrentTime = JSON.parse(currentTimeObj);
-  const currentTimeSeconds = parsedCurrentTime.seconds;
-  console.log(currentTimeSeconds);
+  var currentTimeSeconds = parsedCurrentTime.seconds;
 } catch (error) {
   console.log(error.name);
   console.log(error.message);
 };
 
-player.setCurrentTime(currentTimeSeconds).then(function (seconds) {
-  // seconds = the actual time that the player seeked to
-}).catch(function (error) {
-  switch (error.name) {
-    case 'RangeError':
-      // the time was less than 0 or greater than the video’s duration
-      break;
 
-    default:
-      // some other error occurred
-      break;
-  }
-});
+console.log(currentTimeSeconds);
+player.setCurrentTime(currentTimeSeconds);
+
 
