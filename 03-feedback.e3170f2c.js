1e5626e0bd2a53bad1474f7f6ad9493b1bd13943
gogonzogo <114868158+gogonzogo@users.noreply.github.com>
2023-02-10 18:35:45 +0000

    Deploying to gh-pages from @ gogonzogo/goit-js-hw-08@6b47cf98aab1eed8aa2c71f42baf193e03a41fc4 🚀

---
+++ 03-feedback.e3170f2c.js
@@ -0,0 +1,2 @@
+!function(){var t={form:document.querySelector("form")};_.throttle(handleInput,500);t.form.addEventListener("input",(function(t){t.preventDefault()}))}();
+//# sourceMappingURL=03-feedback.e3170f2c.js.map
