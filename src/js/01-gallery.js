a25fdc2ff560b997966ee233671127a4e8a79d0b
Jose Gonzales <jose.gonzales08@ymail.com>
2023-02-20 21:42:22 -0500

    update ;

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -12,7 +12,7 @@ const ref = {
 function galleryMarkup(array) {
   return array.map((item) =>
     `<a class="gallery__link" href="${item.original}"><img class="gallery__image"src="${item.preview}" alt="${item.description}" width="340"/></a>`).join('');
-}
+};
 ref.galleryList.insertAdjacentHTML("afterbegin", galleryMarkup(galleryItems));
 
 ref.galleryList.addEventListener("click", handleClick);
@@ -22,7 +22,7 @@ function handleClick(e) {
   if (clickTarget.nodeName !== "IMG") {
     return;
   };
-  let showLightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay: 250, captionPosition: "bottom"});
+  let showLightbox = new simpleLightbox('.gallery a', {captionsData:"alt", captionDelay: 250, captionPosition: "bottom"});
   showLightbox.on('show.simplelightbox', function () { });
 };
 
